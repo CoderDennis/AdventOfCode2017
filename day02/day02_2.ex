fcb5989b106b5476e909e7b8d79c2816771517f8
Dennis Palmer <dodava@gmail.com>
2017-12-02 00:34:37 -0600

    refactor to function clauses instead of cond

--- day02/day02_2.ex
+++ day02/day02_2.ex
@@ -19,20 +19,21 @@ defmodule Day02_2 do
 
   def row_even_division([], _), do: 0
 
-  def row_even_division([b | rest], a) do
-    result = cond do
-      rem(a, b) == 0 ->
-        div(a, b)
-      rem(b, a) == 0 ->
-        div(b, a)
-      true ->
-        row_even_division(rest, a)
-    end
+  def row_even_division([a | _], b) when rem(a, b) == 0 do
+    div(a, b)
+  end
+
+  def row_even_division([a | _], b) when rem(b, a) == 0 do
+    div(b, a)
+  end
+
+  def row_even_division([a | rest], b) do
+    result = row_even_division(rest, b)
     if result == 0 do
-      row_even_division(rest, b)
+      row_even_division(rest, a)
     else
       result
     end
   end
-
+  
 end
